439cc1dc1eb1e7561b8c912896adbd816c8ba187
Maythee Anegboonlap <null@llun.me>
2016-03-20 11:55:40 +0800

    Forgot update run command

--- docker_images/labs/run.sh
+++ docker_images/labs/run.sh
@@ -2,7 +2,7 @@
 DOCKER_EXIST=$(docker ps -a -f name=labs --format "{{.ID}}")
 if [ -z $DOCKER_EXIST ]; then
   echo "Creating new container"
-  docker run -it --name labs -v ~/.ssh:/root/.ssh debian bash
+  docker run -it --name labs -v ~/.ssh:/root/.ssh llun/labs bash
 else
   echo "Starting labs container"
   docker start -i labs
